e02b097e18fccf1537a75e5f5ca2d588597b0e03
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-08-23 22:19:11 +0400

    [fix] - works with build in messages model of kernel

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
 var sanitaze = require('validator').sanitize;
 
 exports.name = 'kabamPluginPrivateMessage';
-exports.model = {
-  'Message': function (kabam) {
-    var messageSchema = new kabam.mongoose.Schema({
-      'to': kabam.mongoose.Schema.Types.ObjectId,
-      'from': kabam.mongoose.Schema.Types.ObjectId,
-      'created_at': { type: Date, default: Date.now },
-      'message': {type: String, trim: true } //trim whitespaces - http://mongoosejs.com/docs/api.html#schema_string_SchemaString-trim
-    });
-
-    messageSchema.index({
-      to: 1,
-      from: 1,
-      created_at: 1
-    });
-
-    return kabam.mongoConnection.model('messages', messageSchema);
-  }
-};
 
 exports.routes = function (kabam) {
   kabam.app.get('/api/messages', function (request, response) {
     if (request.user) {
       var mesgLimit = request.query['limit'] ? request.query['limit'] : 10,
         mesgOffset = request.query['offset'] ? request.query['offset'] : 0;
-      request.model.Message
-        .find({'to': request.user._id})
-        .skip(mesgOffset)
-        .limit(mesgLimit)
-        .exec(function (err, messages) {
-          if (err) {
-            throw err;
-          }
-          response.json(messages);
-        });
+
+      request.user.getRecentMessages(mesgLimit, mesgOffset, function(err,messages){
+        if(err) throw err;
+        response.json(messages);
+      });
     } else {
       response.send(400);
     }
@@ -43,29 +20,12 @@ exports.routes = function (kabam) {
     if (request.user) {
       var mesgLimit = request.query['limit'] ? request.query['limit'] : 10,
         mesgOffset = request.query['offset'] ? request.query['offset'] : 0;
-
-      request.model.User.findOneByLoginOrEmail(request.params.username, function (err, userFound) {
-        if (err) {
-          throw err;
-        }
-        if (userFound) {
-          request.model.Message
-            .find({
-              $or: [
-                {'to': request.user._id, 'from': userFound._id},
-                {'from': request.user._id, 'to': userFound._id}
-              ]
-            })
-            .skip(mesgOffset)
-            .limit(mesgLimit)
-            .exec(function (err, messages) {
-              if (err) {
-                throw err;
-              }
-              response.json(messages);
-            });
-        } else {
+      request.user.getDialog(request.params.username,mesgLimit,mesgOffset, function(err,messages){
+        if(err && err.message === 'User do not exists!'){
           response.send(404);
+        } else {
+          if(err) throw err;
+          response.json(messages);
         }
       });
     } else {
@@ -76,29 +36,12 @@ exports.routes = function (kabam) {
 
   kabam.app.post('/api/messages/:username', function (request, response) {
     if (request.user) {
-      var text = sanitaze(request.body.text).xss(true); //https://npmjs.org/package/validator - see xss
-      request.model.User.findOneByLoginOrEmail(request.body.username, function (err, userFound) {
-        if (err) {
-          throw err;
-        }
-        if (userFound) {
-          request.model.Message.create({
-            from: request.user._id,
-            to: userFound._id,
-            message: text
-          }, function (err, messageCreated) {
-            if (err) {
-              throw err;
-            }
-            kabam.emit('notify:pm',{
-              'user':userFound,
-              'from':request.user,
-              'message':text
-            });
-            response.send(201);
-          });
+      request.user.sendMessage(request.params.username,request.body.message,function(err){
+        if(err) throw err;
+        if(request.is('json')){
+          response.json(201,{'status':201,'description':'Message is send!'});
         } else {
-          response.send(404);
+          response.redirect('back');
         }
       });
     } else {
