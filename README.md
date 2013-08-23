c68d9f2759041ed555b05ce2acb4b6ae35859e19
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-08-24 01:08:44 +0400

    [test] - test for sending messages [test] - test for recieving recent images

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ Kabam-plugin-private-message
 
 Kabam plugin for sending and recieving private message
 
-It exposes messages mongoose model
+[![Build Status](https://travis-ci.org/mykabam/kabam-plugin-private-message.png)](https://travis-ci.org/mykabam/kabam-plugin-private-message)
+
+
 It exposes 3 routes
 
 GET /api/messages?limit=10&offset=0
@@ -11,6 +13,7 @@ GET /api/messages?limit=10&offset=0
 
 Get all recent messages for current authenticated user, in reverse 
 chronological order - the most recent messages on top.
+Limit and offset can be ommited.
 
 
 GET /api/messages/:username?limit=10&offset=0
@@ -18,7 +21,7 @@ GET /api/messages/:username?limit=10&offset=0
 
 Get all recent messages of dialog between current authenticated user and other one
 with :username. Messages are sorted in chronological order - the most recent on top.
-
+Limit and offset can be ommited.
 
 POST /api/messages/:username
 ==========================
--- test/pm.test.js
+++ test/pm.test.js
@@ -5,60 +5,165 @@ var should = require('should'),
 
 
 var kabam = KabamKernel({
-  'hostUrl':'http://localhost:3011',
-  'mongoUrl':'mongodb://localhost/mwc_dev',
-  'disableCsrf':true
+  'hostUrl': 'http://localhost:3011',
+  'mongoUrl': 'mongodb://localhost/mwc_dev',
+  'disableCsrf': true // NEVER DO IT!
 });
 
 kabam.usePlugin(require('./../index.js'));
 kabam.start(3011);
 
 
-describe('kabam-plugin-private-message',function(){
-  var User1,User2;
-  before(function(done){
+describe('kabam-plugin-private-message', function () {
+  var User1, User2;
+  before(function (done) {
     async.parallel({
-      'user1':function(cb){
+      'user1': function (cb) {
         kabam.model.User.create({
-          'username':'testSpamer1',
-          'email':'testSpamer1@example.org',
-          'emailVerified':true,
-          'profileComplete':true,
-          'isBanned':false
-        },cb);
+          'username': 'testSpamer1',
+          'email': 'testSpamer1@example.org',
+          'emailVerified': true,
+          'profileComplete': true,
+          'isBanned': false
+        }, cb);
       },
-      'user2':function(cb){
+      'user2': function (cb) {
         kabam.model.User.create({
-          'username':'testSpamer2',
-          'email':'testSpamer2@example.org',
-          'emailVerified':true,
-          'profileComplete':true,
-          'isBanned':false
-        },cb);
+          'username': 'testSpamer2',
+          'email': 'testSpamer2@example.org',
+          'emailVerified': true,
+          'profileComplete': true,
+          'isBanned': false
+        }, cb);
       }
-    },function(err,obj){
-      if(err) throw err;
+    }, function (err, obj) {
+      if (err) {
+        throw err;
+      }
       User1 = obj.user1;
       User2 = obj.user2;
       done();
     });
   });
 
+  describe('User1 sends message to User2 by post request', function () {
+    var response, body, event;
+    before(function (done) {
+      kabam.once('notify:pm', function (m) {
+        event = m;
+        setTimeout(done, 1000);
+      });
 
-  it('sends message');
-  it('allows to get messages');
+      request({
+          'url': 'http://localhost:3011/api/messages/testSpamer2',
+          'method': 'POST',
+          'json': {
+            'mwckey': User1.apiKey, //authorize as User1
+            'message': 'test1'
+          }
+        },
+        function (err, r, b) {
+          if (err) {
+            throw err;
+          }
+          response = r;
+          body = b;
+        });
+    });
 
-  after(function(done){
+    it('he receives proper response for it', function () {
+      response.statusCode.should.be.equal(201);
+      body.should.be.eql({'status': 201, 'description': 'Message is send!'});
+    });
+
+    it('event is emitted once when user sends message', function () {
+      should.exist(event);
+    });
+    it('event have correct "from" field', function () {
+      event.from._id.should.be.eql(User1._id);
+    });
+    it('event have correct "user" field', function () {
+      event.user._id.should.be.eql(User2._id);
+    });
+    it('event have proper contents', function () {
+      event.message.should.be.equal('test1');
+    });
+  });
+
+  describe('User2 recieves his recent messages',function(){
+    var response, body;
+    before(function(done){
+    request({
+        'url': 'http://localhost:3011/api/messages?mwckey='+User2.apiKey,
+        'method': 'GET'
+      },
+      function (err, r, b) {
+        if (err) {
+          throw err;
+        }
+        response = r;
+        body = b;
+        done();
+      });
+    });
+
+    it('he receives proper response for it', function () {
+      response.statusCode.should.be.equal(200);
+      var messages=JSON.parse(body);
+      messages.should.be.instanceOf(Array);
+      messages.length.should.be.equal(1);
+
+      messages[0].to.should.be.eql(User2._id.toString());
+      messages[0].from.should.be.eql(User1._id.toString());
+      messages[0].message.should.be.equal('test1');
+
+    });
+
+  });
+/*/
+  describe('User2 recieves dialog with User1',function(){
+    var response, body;
+    before(function(done){
+      request({
+          'url': 'http://localhost:3011/api/messages/User1?mwckey='+User2.apiKey,
+          'method': 'GET'
+        },
+        function (err, r, b) {
+          if (err) {
+            throw err;
+          }
+          response = r;
+          body = b;
+          done();
+        });
+    });
+
+    it('he receives proper response for it', function () {
+      response.statusCode.should.be.equal(200);
+      var messages=JSON.parse(body);
+      messages.should.be.instanceOf(Array);
+      messages.length.should.be.equal(1);
+
+      messages[0].to.should.be.eql(User2._id.toString());
+      messages[0].from.should.be.eql(User1._id.toString());
+      messages[0].message.should.be.equal('test1');
+
+    });
+
+  });
+//*/
+  after(function (done) {
     async.parallel([
-      function(cb){
+      function (cb) {
         User1.remove(cb);
       },
-      function(cb){
+      function (cb) {
         User2.remove(cb);
       },
-      function(cb){
-        kabam.model.Message.remove({'from':User1._id},cb);
+      function (cb) {
+        kabam.model.Message.remove({'from': User1._id}, cb);
       }
-    ],done);
+    ], done);
   });
+
 });
